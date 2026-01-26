d9cc890c3dc78ed0fd0e1780911b303fd3e81257
Piarre <pierreide10@gmail.com>
2026-01-26 08:53:28 +0100

    feat: add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 /** biome-ignore-all lint/correctness/useExhaustiveDependencies: need to refresh categories on open */
-import { Settings } from "lucide-react";
 import { useMemo, useState } from "react";
-import AddShortcutCard from "@/components/shortcut/add";
 import { ThemeProvider } from "@/components/theme-provider";
-import { Button } from "@/components/ui/button";
 import { Toaster } from "@/components/ui/sonner";
-import AddCategoryCard from "./components/category/add";
 import CategoryCard from "./components/category/card";
 import ShortcutCard from "./components/shortcut/card";
 import { getData } from "./lib/storage";
 import type { Category } from "./types/category";
 import type { Shortcut } from "./types/shortcut";
+import Header from "./components/header";
 
 export function App() {
   const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
@@ -31,13 +28,7 @@ export function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className="h-screen w-screen bg-background text-foreground flex flex-col p-8">
-        <header className="flex  items-center justify-center flex-row gap-4">
-          <Button variant="outline" size="icon" disabled>
-            <Settings />
-          </Button>
-          <AddCategoryCard onSuccess={() => setRefreshKey(prev => prev + 1)} />
-          <AddShortcutCard onSuccess={() => setRefreshKey(prev => prev + 1)} />
-        </header>
+        <Header setRefreshKey={setRefreshKey} />
 
         {categories.length > 0 && (
           <div className="mt-4 flex w-full max-w-3xl gap-2 overflow-x-auto">
@@ -60,7 +51,7 @@ export function App() {
 
         <main className="mt-10 grid grid-cols-3 gap-5">
           {filteredShortcuts.map((shortcut) => (
-            <ShortcutCard key={shortcut.id} {...shortcut} onSuccess={() => setRefreshKey(prev => prev + 1)} />
+            <ShortcutCard key={shortcut.id} {...shortcut} onSuccess={() => setRefreshKey((prev) => prev + 1)} />
           ))}
         </main>
       </div>
---
+++ src/components/header.tsx
@@ -0,0 +1,22 @@
+import { Settings } from "lucide-react";
+import { Button } from "./ui/button";
+import AddCategoryCard from "./category/add";
+import AddShortcutCard from "./shortcut/add";
+
+interface HeaderProps {
+  setRefreshKey: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const Header = ({ setRefreshKey }: HeaderProps) => {
+  return (
+    <header className="flex  items-center justify-center flex-row gap-4">
+      <Button variant="outline" size="icon" disabled>
+        <Settings />
+      </Button>
+      <AddCategoryCard onSuccess={() => setRefreshKey((prev) => prev + 1)} />
+      <AddShortcutCard onSuccess={() => setRefreshKey((prev) => prev + 1)} />
+    </header>
+  );
+};
+
+export default Header;
